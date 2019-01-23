cb61c3e3d9303282262846f9201f635279cf7d09
Lucas Dutra <ldutra98@gmail.com>
2019-01-23 20:52:58 -0200

    Add D logic resolution, but without recommended output

--- simulado1/D.cpp
+++ simulado1/D.cpp
@@ -2,17 +2,38 @@
 using namespace std;
 using vec = vector<int>;
 
+int MAX_N = 10000;
+int counting[10000];
+
+void erase_counting() {
+    for (size_t i = 0; i < MAX_N; i++) {
+      counting[i] = 0;
+    }
+}
+
+
 int main () {
-  int particip, voto;
+  int particip, voto, idVencedor = 0, qtdVotosVencedor = 0;
   set<int> cand;
 
   cin >> particip;
   while (particip != 0) {
     for (size_t i = 0; i < particip; i++) {
       cin >> voto;
+      counting[voto]++;
       cand.insert(voto);
     }
-    cout << cand.size() << endl;
+    for (size_t j = 0; j < MAX_N; j++) {
+      if (counting[j] > qtdVotosVencedor) {
+        qtdVotosVencedor = counting[j];
+        idVencedor = j;
+      }
+    }
+    cout << "Mais votado foi " << idVencedor << " com " << qtdVotosVencedor << " votos" << endl;
+    cout << "No total foram " << cand.size() << " candidatos" << endl;
+    erase_counting();
+    idVencedor = 0;
+    qtdVotosVencedor = 0;
     cin >> particip;
   }
   return 0;
