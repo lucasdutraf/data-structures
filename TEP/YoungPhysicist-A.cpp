a7728fd5a253cb784326e2c27225b3cd6f20c49b
Lucas Dutra <ldutra98@gmail.com>
2019-01-28 08:46:14 -0200

    Add Young Physicist answer

--- TEP/YoungPhysicist-A.cpp
+++ TEP/YoungPhysicist-A.cpp
@@ -5,21 +5,24 @@ using vec = vector <int>;
 int main () {
 
   vector<vec> coordinates;
+  vec aux;
   int cases, positions, sumx = 0, sumy = 0, sumz = 0;
   cin >> cases;
   for (size_t i = 0; i < cases; i++) {
-    coordinates.resize(cases);
     for (size_t j = 0; j < 3; j++) {
       cin >> positions;
-      coordinates.push_back(positions);
+      aux.push_back(positions);
     }
+    coordinates.push_back(aux);
+    aux.clear();
   }
   for (size_t k = 0; k < coordinates.size(); k++) {
-    sumx += coordinates[k][1];
-    sumy += coordinates[k][2];
-    sumz += coordinates[k][3];
+    sumx += coordinates[k][0];
+    sumy += coordinates[k][1];
+    sumz += coordinates[k][2];
   }
-  if ((sumx && sumy && sumz) == 0) {
+
+  if ((sumx == 0) && (sumy == 0) && (sumz == 0)) {
     cout << "YES" << endl;
   }
   else {
