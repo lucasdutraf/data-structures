ccdcd4a061d0fa9d739b687baee414e227fd9a36
Lucas Dutra <ldutra98@gmail.com>
2019-01-29 19:52:09 -0200

    Rename TEP and uri folder

---
+++ TEP-Contests/twoIntegers.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <numeric>
+using namespace std;
+
+int main () {
+
+  int a, b;
+
+  cin >> a >> b;
+
+  auto c = lcm(a, b);
+
+  cout << c << endl;
+
+  return 0;
+}
